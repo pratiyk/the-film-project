844c6926b9e1256806f05251ee4e8c143519fe91
Prateek <pratiyk.shrivastava@gmail.com>
2024-05-11 03:13:01 +0530

    minor additions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-     </div>
+    <Router>
+      <div className="App">
+            <Routes>
+                <Route index element={<h1>hello world</h1>}></Route>
+                <Route></Route>
+                <Route></Route>
+                <Route></Route>
+            </Routes>
+      </div>
+    </Router>
   );
 }
 
